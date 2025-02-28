e3f0aa7cac576ee481f81d89183b5792b023baef
dominick.alvarez <dominickpatriciaalvarezr@gmail.com>
2025-02-28 03:01:43 -0500

    Updates

---
+++ _next/static/chunks/pages/landing/home-e0030ff21898ee2a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[473],{1790:(_,n,e)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/landing/home",function(){return e(756)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[273,756,636,593,792],()=>n(1790)),_N_E=_.O()}]);
